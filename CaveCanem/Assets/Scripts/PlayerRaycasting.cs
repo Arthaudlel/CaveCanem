ec1100e9094168bb315849d6db1fd52cf4572d7e
Kikuchii <benjamin.chambolle@epitech.eu>
2019-03-19 16:08:53 +0100

    [ADD] affichage UI Fiche-infos

--- CaveCanem/Assets/Scripts/PlayerRaycasting.cs
+++ CaveCanem/Assets/Scripts/PlayerRaycasting.cs
@@ -10,24 +10,42 @@ public class PlayerRaycasting : MonoBehaviour {
 
     public float distanceToSee;
     RaycastHit whatIHit;
+    GameObject InformationUi;
+    bool IsPaused = false;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        InformationUi = GameObject.Find("MyInformationsPaper");
+        InformationUi.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
         Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.yellow);
         if (Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee))
         {
             //Debug.Log("Itouched something" + whatIHit.collider.gameObject.name);
-            if (whatIHit.collider.tag == "Terminal")
+            if (whatIHit.collider.tag == "Door")
             {
                 GameObject.Find("InteractionText").GetComponent<Text>().text = "Press E to Use";
-                if (Input.GetKeyDown(KeyCode.E))
+                //if (Input.GetKeyDown(KeyCode.E))
+                if (Input.GetKeyDown(KeyCode.Keypad0))
                 {
-                    //whatIHit.collider.gameObject.GetComponent<OpeningDoor>().opening = true;
+                    whatIHit.collider.gameObject.GetComponent<OpeningDoor>().opening = true;
+                }
+            }
+            else if (whatIHit.collider.tag == "InformationsPaper")
+            {
+                GameObject.Find("InteractionText").GetComponent<Text>().text = "Press 0 to Read";
+                if (Input.GetKeyDown(KeyCode.Keypad0))
+                {
+                    InformationUi.SetActive(true);
+                    this.GetComponent<camMouseLook>().sensitivity = 0f;
+                }
+                if (Input.GetKeyDown(KeyCode.Escape) && InformationUi.activeSelf)
+                {
+                    this.GetComponent<camMouseLook>().sensitivity = 3f;
+                    InformationUi.SetActive(false);
                 }
             }
             else
