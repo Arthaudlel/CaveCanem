7fce8c3366418a151788d3f8e34564d9a1e35699
Arthaudlel <Arthaudlel@gmail.com>
2019-03-20 03:47:10 +0100

    [MOD] Added a Video parameter WIP

---
+++ CaveCanem/Assets/Scripts/ScreenConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenConfig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setVideoMode(int valueScreen)
+    {
+        if (valueScreen == 0)
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        } else if (valueScreen == 1)
+        {
+
+            Screen.fullScreen = Screen.fullScreen;
+        }
+    }
+}
